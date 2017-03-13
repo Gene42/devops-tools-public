c07a5c97742853bc8721e9ac168b61d164cb3e84
Sebastian Stanisor <sebialex@gmail.com>
2017-03-13 18:39:37 -0400

    INFRA-45: fix to known_hosts printf

--- init.sh
+++ init.sh
@@ -47,9 +47,9 @@ known_host_file="${ssh_folder}/known_hosts"
 github_host="github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=="
 
 if [ ! -f "$known_host_file" ]; then
-    printf "%s\n"  > "$known_host_file"
+    printf "%s\n" "$github_host"  > "$known_host_file"
 elif [ -z "$(cat $known_host_file | grep github.com | cat)" ]; then    
-    printf "%s\n"  >> "$known_host_file"
+    printf "%s\n" "$github_host" >> "$known_host_file"
 fi
 
 gene42_dir=~/.gene42
